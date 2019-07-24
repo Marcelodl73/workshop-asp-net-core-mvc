c3ba839ba4f7bed5463fcbd630dce429434a8abf
Marcelo da Luz <marcelodl73@gmail.com>
2019-07-24 11:55:21 -0300

    Implementação de Validação nos campos, operações assíncronas, tratamento de excessões de integridades, página de pesquisa simples e agrupada

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -7,17 +7,24 @@ namespace SalesWebMVC.Models
 {
     public class Seller
     {
-        public int Id { get; set; }
+        public int Id { get; set; } 
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} deve ter entre {2} e {1} caracteres")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [EmailAddress(ErrorMessage = "Entre com um e-mail válido")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Data de Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Required(ErrorMessage = "{0} required")]
+        [Range(100.0, 50000.0, ErrorMessage = "{0} must be from {1} to {2}")]
         [Display(Name = "Salário Base")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
